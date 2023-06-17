022edd402c639b7c9b1fac5845b86a2fdc165cb2
jalallinux <jalallinux@users.noreply.github.com>
2023-06-17 18:42:48 +0000

    Fix styling

--- src/Pm2.php
+++ src/Pm2.php
@@ -14,8 +14,6 @@ class Pm2
     }
 
     /**
-     * @param  string  $sortField
-     * @param  bool  $desc
      * @return array<Process>
      */
     public function list(string $sortField = 'name', bool $desc = true): array
@@ -23,12 +21,6 @@ class Pm2
         return array_map(static fn ($rec) => Process::fromJson($rec), $this->json($sortField, $desc));
     }
 
-    /**
-     * @param  string  $publicKey
-     * @param  string  $secretKey
-     * @param  string|null  $machineName
-     * @return bool
-     */
     public function link(string $publicKey, string $secretKey, string $machineName = null): bool
     {
         $result = $this->runCommand("link {$secretKey} {$publicKey} {$machineName} --update-env");
@@ -36,9 +28,6 @@ class Pm2
         return strpos($result, 'activated!') !== false;
     }
 
-    /**
-     * @return bool
-     */
     public function unlink(): bool
     {
         $result = $this->runCommand('link delete --update-env');
@@ -46,11 +35,6 @@ class Pm2
         return strpos($result, 'ended') !== false;
     }
 
-    /**
-     * @param  string|null  $command
-     * @param  array  $options
-     * @return bool
-     */
     public function start(string $command = null, array $options = []): bool
     {
         $options = $this->makeOptions($options);
@@ -58,11 +42,6 @@ class Pm2
         return ! is_null($this->runCommand('start'.(! is_null($command) ? " \"{$command}\" {$options} --update-env" : '')));
     }
 
-    /**
-     * @param  string  $key
-     * @param  string  $value
-     * @return Process|null
-     */
     public function findBy(string $key, string $value): ?Process
     {
         foreach ($this->list() as $item) {
@@ -74,18 +53,11 @@ class Pm2
         return null;
     }
 
-    /**
-     * @return bool
-     */
     public function kill(): bool
     {
         return ! is_null($this->runCommand('kill --update-env'));
     }
 
-    /**
-     * @param  string  $name
-     * @return int|null
-     */
     public function pid(string $name): ?int
     {
         foreach ($this->list() as $item) {
@@ -97,9 +69,6 @@ class Pm2
         return null;
     }
 
-    /**
-     * @return bool
-     */
     public function flush(): bool
     {
         return ! is_null($this->runCommand('flush --update-env'));
@@ -113,9 +82,6 @@ class Pm2
         return $this->runCommand('update --update-env');
     }
 
-    /**
-     * @return bool
-     */
     public function stopAll(): bool
     {
         if (! is_null($this->runCommand('stop all --update-env'))) {
@@ -127,9 +93,6 @@ class Pm2
         return false;
     }
 
-    /**
-     * @return bool
-     */
     public function restartAll(): bool
     {
         if (! is_null($this->runCommand('restart all --update-env'))) {
@@ -141,9 +104,6 @@ class Pm2
         return false;
     }
 
-    /**
-     * @return bool
-     */
     public function deleteAll(): bool
     {
         if (! is_null($this->runCommand('del all --update-env'))) {
@@ -155,65 +115,36 @@ class Pm2
         return false;
     }
 
-    /**
-     * @param  string  $idOrName
-     * @return bool
-     */
     public function stop(string $idOrName): bool
     {
         return ! is_null($this->runCommand("stop {$idOrName} --update-env"));
     }
 
-    /**
-     * @param  string  $idOrName
-     * @return bool
-     */
     public function restart(string $idOrName): bool
     {
         return ! is_null($this->runCommand("restart {$idOrName} --update-env"));
     }
 
-    /**
-     * @param  string  $idOrName
-     * @return bool
-     */
     public function delete(string $idOrName): bool
     {
         return ! is_null($this->runCommand("delete {$idOrName} --update-env"));
     }
 
-    /**
-     * @param  bool  $force
-     * @return bool
-     */
     public function save(bool $force = true): bool
     {
         return ! is_null($this->runCommand('save'.($force ? ' --force' : '')));
     }
 
-    /**
-     * @param  string|null  $idOrName
-     * @param  int  $lines
-     * @return string
-     */
     public function logOut(string $idOrName = null, int $lines = 100): string
     {
         return $this->runCommand('logs'.(! is_null($idOrName) ? " {$idOrName}" : '')." --lines={$lines} --nostream --raw --out");
     }
 
-    /**
-     * @param  string|null  $idOrName
-     * @param  int  $lines
-     * @return string
-     */
     public function logErr(string $idOrName = null, int $lines = 100): string
     {
         return $this->runCommand('logs'.(! is_null($idOrName) ? " {$idOrName}" : '')." --lines={$lines} --nostream --raw --err");
     }
 
-    /**
-     * @return bool
-     */
     public function startup(): bool
     {
         $this->runCommand('startup --update-env');
@@ -221,16 +152,12 @@ class Pm2
         return $this->save();
     }
 
-    /**
-     * @return string
-     */
     public function version(): string
     {
         return trim($this->runCommand('--version'));
     }
 
     /**
-     * @param  string  $version
      * @return false|string|null
      */
     public function install(string $version = 'latest')
@@ -238,11 +165,6 @@ class Pm2
         return shell_exec("npm install -g pm2@{$version}");
     }
 
-    /**
-     * @param  bool  $forceInstall
-     * @param  string  $version
-     * @return bool
-     */
     public function isInstall(bool $forceInstall = false, string $version = 'latest'): bool
     {
         $isInstall = ! is_null($this->runCommand('--version'));
@@ -269,7 +191,7 @@ class Pm2
 
     protected function runCommand(string $command)
     {
-        if (!is_null($this->prefix)) {
+        if (! is_null($this->prefix)) {
             return shell_exec("{$this->prefix} pm2 {$command}");
         }
 
